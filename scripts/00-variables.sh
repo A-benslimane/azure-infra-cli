247b714abc399197fac9aa3ab89dde851cd481d3
A-benslimane <benslimane201440@gmail.com>
2026-06-26 15:48:10 +0200

    refactor: reorganize variable declarations in 00-variables.sh

--- scripts/00-variables.sh
+++ scripts/00-variables.sh
@@ -3,21 +3,19 @@
 export LOCATION="francecentral"
 export RESOURCE_GROUP="${RESOURCE_GROUP:-abenslimaneRG}"
 
+# Ressources Azure
 export STORAGE_ACCOUNT="stabdelcli6594"
 export APP_SERVICE_PLAN="plan-abdel-cli"
 export WEBAPP_NAME="api-appservice-abdel-4158"
 export FUNCTION_APP_NAME="api-func-abdel-5013"
-
 export ACI_NAME="api-aci-abdel"
 export ACI_DNS_LABEL="api-aci-abdel-4158"
 
-# Variables réseau
-
+# Variables communes
 export OWNER="abdel-benslimane"
-export RG="$RESOURCE_GROUP"
-
 export TAGS="managed_by=cli environment=tp owner=${OWNER}"
 
+# Réseau
 export VNET_NAME="vnet-${OWNER}-cli"
 export NSG_NAME="nsg-frontend-${OWNER}-cli"
 export NIC_NAME="nic-test-${OWNER}-cli"
